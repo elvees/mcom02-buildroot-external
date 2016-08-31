3d2b5bde612a8fd1717c26cc10aaedf478863ba4
Alexey Kiselev <akiselev@elvees.com>
2016-08-31 17:42:37 +0300

    mcom-linux-tests: Change the version to master
    The master version of MCom Linux tests is used now to avoid updating
    of this repository and the repositories, using this as a submodule,
    after updating of the tests.
    
    Change-Id: Ic30568c063dc7654ad15eda48d28a74efd095157

--- package/mcom/mcom-linux-tests/mcom-linux-tests.mk
+++ package/mcom/mcom-linux-tests/mcom-linux-tests.mk
@@ -4,7 +4,7 @@
 #
 #############################################################
 
-MCOM_LINUX_TESTS_VERSION = 36f7fbd4fae0f3805cf7e472fec8bcbfcae49b0d
+MCOM_LINUX_TESTS_VERSION = master
 MCOM_LINUX_TESTS_SITE = ssh://callisto.elvees.com:29418/tools/mcom-linux-tests
 MCOM_LINUX_TESTS_SITE_METHOD = git
 MCOM_LINUX_TESTS_LICENSE = BSD-2c
