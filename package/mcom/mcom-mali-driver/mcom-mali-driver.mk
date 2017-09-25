98ededfe0862d3a064d3e52ee9bf6773f043499e
Andrey Solodovnikov <asolodovnikov@elvees.com>
2017-09-25 17:18:07 +0300

    mcom-mali-driver: Split libraries into mcom-libmali package
    Closed source libMali.so, libUMP.so libraries are moved to lib/mali
    repository. Open source Mali kernel driver remained in
    linux/modules/mali repository.
    
    This commit adds recipe for new mcom-libmali package and updates recipe
    for mcom-mali-driver.
    
    Now, mcom-mali-driver recipe builds kernel modules from source code.
    
    Issue: #MCOM02SW-441
    Change-Id: I0a94120720abf5952ce118cb1a4a9b3a912a5ecf

--- package/mcom/mcom-mali-driver/mcom-mali-driver.mk
+++ package/mcom/mcom-mali-driver/mcom-mali-driver.mk
@@ -4,21 +4,19 @@
 #
 ################################################################################
 
-MCOM_MALI_DRIVER_VERSION = r3p0-04rel0-v2.4
+MCOM_MALI_DRIVER_VERSION = mcom
 MCOM_MALI_DRIVER_DEPENDENCIES = linux
-MCOM_MALI_DRIVER_LICENSE = custom
-MCOM_MALI_DRIVER_SITE = $(BR2_EXTERNAL)/package/mcom/mcom-mali-driver
-MCOM_MALI_DRIVER_SITE_METHOD = local
-MCOM_MALI_DRIVER_LIBS_PREFIX = /lib
+MCOM_MALI_DRIVER_SITE = ssh://callisto.elvees.com:29418/linux/modules/mali
+MCOM_MALI_DRIVER_SITE_METHOD = git
+
+define MCOM_MALI_DRIVER_BUILD_CMDS
+	$(MAKE) -C $(@D) ARCH=$(KERNEL_ARCH) CROSS_COMPILE=$(notdir $(TARGET_CROSS)) KDIR=$(LINUX_DIR)
+endef
 
 define MCOM_MALI_DRIVER_INSTALL_TARGET_CMDS
-	$(INSTALL) -D -m 0644 $(@D)/mali.ko $(TARGET_DIR)/lib/modules/$(LINUX_VERSION_PROBED)/extra/mali.ko
-	$(INSTALL) -D -m 0644 $(@D)/ump.ko $(TARGET_DIR)/lib/modules/$(LINUX_VERSION_PROBED)/extra/ump.ko
+	$(INSTALL) -D -m 0644 $(@D)/devicedrv/mali/mali.ko $(TARGET_DIR)/lib/modules/$(LINUX_VERSION_PROBED)/extra/mali.ko
+	$(INSTALL) -D -m 0644 $(@D)/devicedrv/ump/ump.ko $(TARGET_DIR)/lib/modules/$(LINUX_VERSION_PROBED)/extra/ump.ko
 	$(HOST_DIR)/sbin/depmod -b $(TARGET_DIR) $(LINUX_VERSION_PROBED)
-	$(INSTALL) -D -m 0755 $(@D)/libMali.so $(TARGET_DIR)$(MCOM_MALI_DRIVER_LIBS_PREFIX)/libMali.so
-	$(INSTALL) -D -m 0755 $(@D)/libUMP.so $(TARGET_DIR)$(MCOM_MALI_DRIVER_LIBS_PREFIX)/libUMP.so
-	for f in libEGL.so libEGL.so.1.4 libGLESv1_CM.so.1 libGLESv2.so libGLESv2.so.2.0 libEGL.so.1 libGLESv1_CM.so libGLESv1_CM.so.1.1 libGLESv2.so.2; do ln -sf libMali.so $(TARGET_DIR)$(MCOM_MALI_DRIVER_LIBS_PREFIX)/$$f;done
-	tar -C $(TARGET_DIR) -xf $(@D)/mali-demos.tar.gz
 endef
 
 $(eval $(generic-package))
