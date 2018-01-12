cabb7db49fceeceb6617b89db33603d79ad7c97a
Anton Leontiev <aleontiev@elvees.com>
2018-01-12 19:16:30 +0300

    mcom-libmali: Install libraries to staging
    To build any software that depends on libMali.so and libUMP.so, they
    should be installed to staging directory. We are going to add
    xf86-video-mali driver, which depends on libUMP.so.
    
    Issue: #MCOM02SW-593
    Change-Id: I41659a53be8931b86d3766e7da36c8b80e139a39

--- package/mcom/mcom-libmali/mcom-libmali.mk
+++ package/mcom/mcom-libmali/mcom-libmali.mk
@@ -8,13 +8,18 @@ MCOM_LIBMALI_VERSION = r3p0-04rel0-v2.4
 MCOM_LIBMALI_LICENSE = custom
 MCOM_LIBMALI_SITE = $(BR2_EXTERNAL)/package/mcom/mcom-libmali
 MCOM_LIBMALI_SITE_METHOD = local
+MCOM_LIBMALI_INSTALL_STAGING = YES
 MCOM_LIBMALI_LIBS_PREFIX = /lib
 
-define MCOM_LIBMALI_INSTALL_TARGET_CMDS
-	$(INSTALL) -D -m 0755 $(@D)/libMali.so $(TARGET_DIR)$(MCOM_LIBMALI_LIBS_PREFIX)/libMali.so
-	$(INSTALL) -D -m 0755 $(@D)/libUMP.so $(TARGET_DIR)$(MCOM_LIBMALI_LIBS_PREFIX)/libUMP.so
-	for f in libEGL.so libEGL.so.1.4 libGLESv1_CM.so.1 libGLESv2.so libGLESv2.so.2.0 libEGL.so.1 libGLESv1_CM.so libGLESv1_CM.so.1.1 libGLESv2.so.2; do ln -sf libMali.so $(TARGET_DIR)$(MCOM_LIBMALI_LIBS_PREFIX)/$$f;done
-	tar -C $(TARGET_DIR) -xf $(@D)/mali-demos.tar.gz
+define MCOM_LIBMALI_INSTALL_CMDS
+	$(INSTALL) -D -m 0755 $(@D)/libMali.so $(1)$(MCOM_LIBMALI_LIBS_PREFIX)/libMali.so
+	$(INSTALL) -D -m 0755 $(@D)/libUMP.so $(1)$(MCOM_LIBMALI_LIBS_PREFIX)/libUMP.so
+	for f in libEGL.so{,.1,.1.4} libGLESv1_CM.so{,.1,.1.1} libGLESv2.so{,.2,.2.0}; do \
+		ln -sf libMali.so $(1)$(MCOM_LIBMALI_LIBS_PREFIX)/$$f;done
+	tar -C $(1) -xf $(@D)/mali-demos.tar.gz
 endef
+
+MCOM_LIBMALI_INSTALL_TARGET_CMDS = $(call MCOM_LIBMALI_INSTALL_CMDS,$(TARGET_DIR))
+MCOM_LIBMALI_INSTALL_STAGING_CMDS = $(call MCOM_LIBMALI_INSTALL_CMDS,$(STAGING_DIR))
 
 $(eval $(generic-package))
