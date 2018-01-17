4d2d651da2253f0ed8ef056e7e592362cc6738b8
Dmitriy Zagrebin <dzagrebin@elvees.com>
2018-01-17 17:45:19 +0300

    v4l2tofb: Use master as a package version
    This allows to avoid periodic manual version updates.
    
    Change-Id: I7bfbe676f3d4b34758bcfc68d12fe82260e59ed0

--- package/v4l2tofb/v4l2tofb.mk
+++ package/v4l2tofb/v4l2tofb.mk
@@ -4,7 +4,7 @@
 #
 #############################################################
 
-V4L2TOFB_VERSION = ce198a7c83086676cca7b73e547553665c16725e
+V4L2TOFB_VERSION = master
 V4L2TOFB_SITE = ssh://callisto.elvees.com:29418/tools/v4l2tofb
 V4L2TOFB_SITE_METHOD = git
 V4L2TOFB_LICENSE = GPLv2
