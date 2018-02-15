d03b1b9d8c3f9787bacf3c65d3ef029519ff4243
Anton Leontiev <aleontiev@elvees.com>
2018-02-15 13:50:41 +0300

    mcom-mali-driver: Add necessary environment to build command
    Without this environment the package fails to build if host directory
    is outside of Buildroot top directory.
    
    Change-Id: Idcb2ab14f2091993f2bec339ef6d339c29fbde76

--- package/mcom/mcom-mali-driver/mcom-mali-driver.mk
+++ package/mcom/mcom-mali-driver/mcom-mali-driver.mk
@@ -9,8 +9,9 @@ MCOM_MALI_DRIVER_DEPENDENCIES = linux
 MCOM_MALI_DRIVER_SITE = ssh://callisto.elvees.com:29418/linux/modules/mali
 MCOM_MALI_DRIVER_SITE_METHOD = git
 
+# TODO: Rework repository to use $(kernel-module) infrastructure
 define MCOM_MALI_DRIVER_BUILD_CMDS
-	$(MAKE) -C $(@D) ARCH=$(KERNEL_ARCH) CROSS_COMPILE=$(notdir $(TARGET_CROSS)) KDIR=$(LINUX_DIR)
+	$(TARGET_MAKE_ENV) $(MAKE) -C $(@D) ARCH=$(KERNEL_ARCH) CROSS_COMPILE=$(notdir $(TARGET_CROSS)) KDIR=$(LINUX_DIR)
 endef
 
 define MCOM_MALI_DRIVER_INSTALL_TARGET_CMDS
