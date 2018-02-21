7cb844e39dfa4ed328681133c9c7608d2921b4fc
Vasiliy Zasukhin <vzasukhin@elvees.com>
2018-02-21 20:18:32 +0300

    uart-loopback-test: Use master as a package version
    This allows to avoid periodic manual version updates.
    
    Change-Id: Ic09ddc235b60255f9c947a27ec8623b93aac4269

--- package/uart-loopback-test/uart-loopback-test.mk
+++ package/uart-loopback-test/uart-loopback-test.mk
@@ -6,7 +6,7 @@
 
 UART_LOOPBACK_TEST_SITE = ssh://callisto.elvees.com:29418/tools/uart-loopback-test
 UART_LOOPBACK_TEST_SITE_METHOD = git
-UART_LOOPBACK_TEST_VERSION = 117117e2122620fb413e726fdcd80f5797fd2392
+UART_LOOPBACK_TEST_VERSION = master
 UART_LOOPBACK_TEST_SETUP_TYPE = setuptools
 
 $(eval $(python-package))
