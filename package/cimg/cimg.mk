edd613a2d6b77775ea1465d4e78cfce65d891df2
Igor Sharonov <isharonov@elvees.com>
2018-06-06 15:25:30 +0300

    cimg: New package
    A small and open-source C++ toolkit for image processing.
    
    Issue: #MCOM02SW-798
    Change-Id: I6c32b42d19ca9ea059629519743d4bdd7dcabb74

---
+++ package/cimg/cimg.mk
@@ -0,0 +1,23 @@
+################################################################################
+#
+# cimg
+#
+################################################################################
+
+CIMG_VERSION = 2.2.2
+CIMG_SITE = http://cimg.eu/files
+CIMG_SOURCE = CImg_$(CIMG_VERSION).zip
+CIMG_LICENSE = CeCILL-C
+CIMG_INSTALL_STAGING = YES
+CIMG_INSTALL_TARGET = NO
+CIMG_DEPENDENCIES = libpng jpeg
+
+define CIMG_EXTRACT_CMDS
+	$(UNZIP) -d $(@D) $(DL_DIR)/$(CIMG_SOURCE)
+endef
+
+define CIMG_INSTALL_STAGING_CMDS
+	$(INSTALL) -D -m 0644 $(@D)/CImg-$(CIMG_VERSION)/CImg.h $(STAGING_DIR)/include/CImg.h
+endef
+
+$(eval $(generic-package))
