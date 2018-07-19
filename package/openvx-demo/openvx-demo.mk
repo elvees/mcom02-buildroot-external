081fbe7ae5768c06b1a38a22230b1180004ff535
Igor Sharonov <isharonov@elvees.com>
2018-07-19 13:04:55 +0300

    openvx-demo: New package
    A bunch of samples of OpenVX library usage.
    
    Issue: #MCOM02SW-798
    Change-Id: Ia33a391865e0a4710ee2d1bf7066f01d470a7d21

---
+++ package/openvx-demo/openvx-demo.mk
@@ -0,0 +1,16 @@
+################################################################################
+#
+# openvx-demo
+#
+################################################################################
+
+OPENVX_DEMO_VERSION = 0.1.2
+OPENVX_DEMO_SOURCE = openvx_demo-v$(OPENVX_DEMO_VERSION).tar.gz
+OPENVX_DEMO_LICENSE = custom
+OPENVX_DEMO_SITE = https://diver.elvees.com/share/openvx_demo/-/archive/v$(OPENVX_DEMO_VERSION)
+OPENVX_DEMO_DEPENDENCIES = openvx cimg
+
+$(eval $(cmake-package))
+
+# No $(PKG)_DL_OPTS is available. Use target-specific variable
+$(BUILD_DIR)/$(OPENVX_DEMO_NAME)-$(OPENVX_DEMO_VERSION)/.stamp_downloaded: WGET += --no-check-certificate
