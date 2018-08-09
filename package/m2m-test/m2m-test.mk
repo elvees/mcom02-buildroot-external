4d52f5a8c6699eb86031c505d4c0dc0f2a92df90
Vitaliy Lotorev <vlotorev@elvees.com>
2018-08-09 12:57:05 +0300

    m2m-test: Use latest version from master branch
    MCom-02 Buildroot needs to use latest version.
    
    FFmpeg was updated to v3.4.2 when Buildroot was upgraded to v2018.02
    
    Issue: #MCOM02SW-897
    Change-Id: I9120d4499133a542222670334861cc4a5121ce06

--- package/m2m-test/m2m-test.mk
+++ package/m2m-test/m2m-test.mk
@@ -4,7 +4,7 @@
 #
 #############################################################
 
-M2M_TEST_VERSION = v1.3
+M2M_TEST_VERSION = master
 M2M_TEST_SITE = ssh://callisto.elvees.com:29418/tools/m2m-test
 M2M_TEST_SITE_METHOD = git
 M2M_TEST_LICENSE = GPLv2
