478176dca6731c4884d0236048dbf61331546223
Anton Leontiev <aleontiev@elvees.com>
2018-08-10 11:39:03 +0300

    coremark: Override release compilation flags
    The purpose of coremark is to benchmark the CPU, so it must always
    be compiled with enabled optimizations regardless of Buildroot
    configuration.
    
    Issue: #MCOM02SW-898
    Change-Id: I1870015fd08836558b7886362d1a1c6efbbe38b4

--- package/coremark/coremark.mk
+++ package/coremark/coremark.mk
@@ -7,6 +7,6 @@
 COREMARK_VERSION = master
 COREMARK_SITE = ssh://callisto.elvees.com:29418/tools/coremark
 COREMARK_SITE_METHOD = git
-COREMARK_CONF_OPTS = -DTHREADS=2
+COREMARK_CONF_OPTS = -DTHREADS=2 -DCMAKE_C_FLAGS_RELEASE="-Ofast -mcpu=$(BR2_GCC_TARGET_CPU) -DNDEBUG"
 
 $(eval $(cmake-package))
