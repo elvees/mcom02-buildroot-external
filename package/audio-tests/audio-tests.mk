5d6b2f828a9681ada2939373e71fb394c83673a6
Vasiliy Zasukhin <vzasukhin@elvees.com>
2018-09-20 11:27:14 +0300

    audio-tests: New package
    Audio-tests package contains an utility to test audio
    input/output. It analyzes WAV file and print amplitude,
    frequency and noise level for each channel.
    
    Issue: #MCOM02SW-828
    Change-Id: I6b9615a58dfb1aac6f1da87ee1055e1cb80fa183

---
+++ package/audio-tests/audio-tests.mk
@@ -0,0 +1,13 @@
+################################################################################
+#
+# audio-tests
+#
+################################################################################
+
+AUDIO_TESTS_SITE = ssh://callisto.elvees.com:29418/tools/audio-tests
+AUDIO_TESTS_SITE_METHOD = git
+AUDIO_TESTS_VERSION = master
+AUDIO_TESTS_SETUP_TYPE = setuptools
+AUDIO_TESTS_DEPENDENCIES = python-numpy
+
+$(eval $(python-package))
