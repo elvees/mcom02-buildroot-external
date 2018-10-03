248f88f2adf6f0b73e43f188ebfbaef26f89a04b
Georgy Macharadze <gmacharadze@elvees.com>
2018-10-03 09:46:23 +0300

    openvx: Fix header installation path
    '/usr' part is added to headers installation path because /usr is
    default prefix in Buildroot.
    
    Issue: #MCOM02SW-798
    Change-Id: Ieab41ec21649930dd8b3995d00454dffe321df02

--- package/openvx/openvx.mk
+++ package/openvx/openvx.mk
@@ -13,14 +13,14 @@ OPENVX_INSTALL_STAGING = YES
 OPENVX_DEPENDENCIES = openvx-headers
 
 define OPENVX_INSTALL_CMD
-	$(INSTALL) -D -m 0755 $(@D)/libopenvx.so.$(OPENVX_VERSION) $(1)/lib/libopenvx.so.$(OPENVX_VERSION)
-	ln -sf libopenvx.so.$(OPENVX_VERSION) $(1)/lib/libopenvx.so
-	ln -sf libopenvx.so.$(OPENVX_VERSION) $(1)/lib/libopenvx.so.$(OPENVX_MAJOR_VERSION)
+	$(INSTALL) -D -m 0755 $(@D)/libopenvx.so.$(OPENVX_VERSION) $(1)/usr/lib/libopenvx.so.$(OPENVX_VERSION)
+	ln -sf libopenvx.so.$(OPENVX_VERSION) $(1)/usr/lib/libopenvx.so
+	ln -sf libopenvx.so.$(OPENVX_VERSION) $(1)/usr/lib/libopenvx.so.$(OPENVX_MAJOR_VERSION)
 endef
 
 define OPENVX_INSTALL_STAGING_CMDS
 	$(call OPENVX_INSTALL_CMD,$(STAGING_DIR))
-	$(INSTALL) -D -m 0644 $(@D)/openvx-extended.h $(STAGING_DIR)/include/openvx_extended.h
+	$(INSTALL) -D -m 0644 $(@D)/openvx-extended.h $(STAGING_DIR)/usr/include/openvx_extended.h
 endef
 
 OPENVX_INSTALL_TARGET_CMDS = $(call OPENVX_INSTALL_CMD,$(TARGET_DIR))
