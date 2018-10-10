ce4f9d9beb3dbe0e0149ef1c6177385badfc7d99
Olzhas Ivanov <oivanov@elvees.com>
2018-10-10 21:50:08 +0300

    mcom-linux-tests: Add missing dependency on dmatestcontig
    Dependency is required by dmatestcontig.sh for its operation.
    
    Change-Id: I839e8771561513299b55b5837d848845f48abae5

--- package/mcom/mcom-linux-tests/mcom-linux-tests.mk
+++ package/mcom/mcom-linux-tests/mcom-linux-tests.mk
@@ -8,5 +8,6 @@ MCOM_LINUX_TESTS_VERSION = master
 MCOM_LINUX_TESTS_SITE = ssh://callisto.elvees.com:29418/tools/mcom-linux-tests
 MCOM_LINUX_TESTS_SITE_METHOD = git
 MCOM_LINUX_TESTS_LICENSE = BSD-2c
+MCOM_LINUX_TESTS_DEPENDENCIES = dmatestcontig
 
 $(eval $(cmake-package))
