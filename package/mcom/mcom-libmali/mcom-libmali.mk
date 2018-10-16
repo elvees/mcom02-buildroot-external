a0261bf875edf82ab8ed37989e29ec93320cc420
Dmitriy Zagrebin <dzagrebin@elvees.com>
2018-10-16 18:09:35 +0300

    mcom-libmali: Get libMali.so from bin/mali repo
    Actual version of libMali.so is now located in bin/mali repo.
    
    libUMP.so and mali-demos are moved into separate packages.
    
    Issue: #MCOM02SW-839
    Change-Id: If435a1fe819b819822985e80d7aa8bc8ccd4dc60

--- package/mcom/mcom-libmali/mcom-libmali.mk
+++ package/mcom/mcom-libmali/mcom-libmali.mk
@@ -4,19 +4,15 @@
 #
 ################################################################################
 
-MCOM_LIBMALI_VERSION = r3p0-04rel0-v2.4
+MCOM_LIBMALI_VERSION = mcom02
 MCOM_LIBMALI_LICENSE = custom
-MCOM_LIBMALI_SITE = $(BR2_EXTERNAL)/package/mcom/mcom-libmali
-MCOM_LIBMALI_SITE_METHOD = local
+MCOM_LIBMALI_SITE = ssh://callisto.elvees.com:29418/bin/mali
+MCOM_LIBMALI_SITE_METHOD = git
 MCOM_LIBMALI_INSTALL_STAGING = YES
-MCOM_LIBMALI_LIBS_PREFIX = /lib
 
 define MCOM_LIBMALI_INSTALL_CMDS
-	$(INSTALL) -D -m 0755 $(@D)/libMali.so $(1)$(MCOM_LIBMALI_LIBS_PREFIX)/libMali.so
-	$(INSTALL) -D -m 0755 $(@D)/libUMP.so $(1)$(MCOM_LIBMALI_LIBS_PREFIX)/libUMP.so
-	for f in libEGL.so{,.1,.1.4} libGLESv1_CM.so{,.1,.1.1} libGLESv2.so{,.2,.2.0}; do \
-		ln -sf libMali.so $(1)$(MCOM_LIBMALI_LIBS_PREFIX)/$$f;done
-	tar -C $(1) -xf $(@D)/mali-demos.tar.gz
+	$(TARGET_MAKE_ENV) $(MAKE) -C $(MCOM_LIBMALI_SRCDIR) DESTDIR=$(1) \
+		VARIANT=mali300-gles11-gles20-linux-ump-drawmerge-no_profiling install
 endef
 
 MCOM_LIBMALI_INSTALL_TARGET_CMDS = $(call MCOM_LIBMALI_INSTALL_CMDS,$(TARGET_DIR))
