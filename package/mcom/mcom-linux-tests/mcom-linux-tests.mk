61f183e611b96b9fbbd83bfe64fad3f930c5042f
Olzhas Ivanov <oivanov@elvees.com>
2019-04-25 22:03:29 +0300

    mcom-linux-tests: Remove useless dependencies
    Dependencies in mk file determine packages necessary during the building
    process of target package.
    
    The removed dependencies are not required for building of the
    mcom-linux-tests.
    
    Change-Id: Ib6d774d78a51bae7fb484c574adec5c614729dc4

--- package/mcom/mcom-linux-tests/mcom-linux-tests.mk
+++ package/mcom/mcom-linux-tests/mcom-linux-tests.mk
@@ -8,7 +8,5 @@ MCOM_LINUX_TESTS_VERSION = master
 MCOM_LINUX_TESTS_SITE = ssh://gerrit.elvees.com:29418/tools/mcom-linux-tests
 MCOM_LINUX_TESTS_SITE_METHOD = git
 MCOM_LINUX_TESTS_LICENSE = BSD-2c
-MCOM_LINUX_TESTS_DEPENDENCIES = dmatestcontig memtester-simple mcom-mali-demos \
-				python
 
 $(eval $(cmake-package))
