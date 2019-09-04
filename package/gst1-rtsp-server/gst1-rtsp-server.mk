6314cc9b27777a6fcf73ad73887e84f9d26c175c
Anton Leontiev <aleontiev@elvees.com>
2019-09-04 12:58:59 +0300

    gst1-rtsp-server: Enable examples in configuration options
    Buildroot 2019.08 explicitly disables examples and thus installation
    step fails because no test-launch file exists.
    
    Change-Id: I64dc435a2aca157dde2fcf9ab45c4085aa89684a

--- package/gst1-rtsp-server/gst1-rtsp-server.mk
+++ package/gst1-rtsp-server/gst1-rtsp-server.mk
@@ -4,6 +4,8 @@
 #
 ################################################################################
 
+GST1_RTSP_SERVER_CONF_OPTS += --enable-examples
+
 define GST1_RTSP_SERVER_POST_INSTALL_TEST_LAUNCH
 	$(INSTALL) -D -m 0755 $(@D)/examples/test-launch $(TARGET_DIR)/usr/bin/gst-rtsp-test-launch
 endef
