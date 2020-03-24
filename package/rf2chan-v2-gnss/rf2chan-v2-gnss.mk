bca26133e389dcc90094bc37e2db3e1ada729ba5
Pavel Romanjuk <promanjuk@elvees.com>
2020-03-24 19:42:30 +0300

    rf2chan-v2-gnss: New package
    rfchan-v2-gnss is a kernel module for RF2Chan v2 GNNS front end module.
    
    Issue: #MCOM02SW-1764
    Change-Id: I6912b47525a4d670a0bf87dac359da6f3c73edd4

---
+++ package/rf2chan-v2-gnss/rf2chan-v2-gnss.mk
@@ -0,0 +1,13 @@
+################################################################################
+#
+# rf2chan-v2-gnss
+#
+################################################################################
+
+RF2CHAN_V2_GNSS_VERSION = master
+RF2CHAN_V2_GNSS_SITE = ssh://git@git.elvees.com/gnss/mcom02-linux/el24d1-rf2chan-gnss-kernel-driver.git
+RF2CHAN_V2_GNSS_SITE_METHOD = git
+RF2CHAN_V2_GNSS_LICENSE = GPL
+
+$(eval $(kernel-module))
+$(eval $(generic-package))
