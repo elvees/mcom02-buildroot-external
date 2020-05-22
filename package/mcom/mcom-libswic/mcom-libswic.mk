dafa0ce00007f7ebd9f44946f16a29239ba1ac56
Oleg Ivanov <oaivanov@elvees.com>
2020-05-22 21:53:24 +0300

    libswic: Add preset file for spacewire video service
    Starting from v2020.02 Buildroot automatically enables all systemd
    services using the systemctl preset-all command in the final phase
    of image building.
    
    This commit add preset file to prevent a spacewire video service from
    being automatically enabled by Buildroot.
    
    Issue: #MCOM02SW-1799
    Change-Id: Icb357c4aa71c53fcd84a3cac21a562f0bb316cdd

--- package/mcom/mcom-libswic/mcom-libswic.mk
+++ package/mcom/mcom-libswic/mcom-libswic.mk
@@ -8,4 +8,10 @@ MCOM_LIBSWIC_SITE = ssh://gerrit.elvees.com:29418/lib/swic
 MCOM_LIBSWIC_SITE_METHOD = git
 MCOM_LIBSWIC_VERSION = master
 
+# Disable the service by default.
+define MCOM_LIBSWIC_INSTALL_INIT_SYSTEMD
+	$(INSTALL) -D -m 0644 $(MCOM_LIBSWIC_PKGDIR)/50-spacewire.preset \
+		$(TARGET_DIR)/usr/lib/systemd/system-preset/50-spacewire.preset
+endef
+
 $(eval $(cmake-package))
