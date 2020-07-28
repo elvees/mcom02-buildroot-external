fb78df28f4fc56e5f27c570f1d61587a34db8f9c
Pavel Romanjuk <promanjuk@elvees.com>
2020-07-28 12:19:27 +0300

    rf2chan-v2-gnss: Change repo path
    Set explicit ssh port (22) to the repository path.
    
    Issue: #MCOM02SW-1764
    Change-Id: I9d85cb5ba22f690fbf46196228d1f3d64d00d49c

--- package/rf2chan-v2-gnss/rf2chan-v2-gnss.mk
+++ package/rf2chan-v2-gnss/rf2chan-v2-gnss.mk
@@ -5,7 +5,7 @@
 ################################################################################
 
 RF2CHAN_V2_GNSS_VERSION = master
-RF2CHAN_V2_GNSS_SITE = ssh://git@git.elvees.com/gnss/mcom02-linux/el24d1-rf2chan-gnss-kernel-driver.git
+RF2CHAN_V2_GNSS_SITE = ssh://git@git.elvees.com:22/gnss/mcom02-linux/el24d1-rf2chan-gnss-kernel-driver.git
 RF2CHAN_V2_GNSS_SITE_METHOD = git
 RF2CHAN_V2_GNSS_LICENSE = GPL
 
