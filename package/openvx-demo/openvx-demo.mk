37254c53c5e2219d3b3d9f9e8d528fa5f0d2bf19
Igor Sharonov <isharonov@elvees.com>
2020-09-22 15:00:09 +0300

    package/openvx-demo: Update version to 0.2.0
    Issue: #MCOM02SW-1727
    Change-Id: I2dee2fedd3325607fcf7dca4e91376d8f1708c03

--- package/openvx-demo/openvx-demo.mk
+++ package/openvx-demo/openvx-demo.mk
@@ -4,11 +4,12 @@
 #
 ################################################################################
 
-OPENVX_DEMO_VERSION = 0.1.2
+OPENVX_DEMO_VERSION = 0.2.0
 OPENVX_DEMO_SOURCE = openvx_demo-v$(OPENVX_DEMO_VERSION).tar.gz
 OPENVX_DEMO_LICENSE = custom
 OPENVX_DEMO_SITE = https://warlock.elvees.com/share/openvx_demo/-/archive/v$(OPENVX_DEMO_VERSION)
 OPENVX_DEMO_DEPENDENCIES = openvx cimg
+OPENVX_DEMO_CONF_OPTS = -DOPENVX_LIBS="libopenvx_wrapper.so;libopenvx_kernels.so"
 
 $(eval $(cmake-package))
 
