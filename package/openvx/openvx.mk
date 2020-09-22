744e6eadff3cf58647676e68455dc46f36066099
Igor Sharonov <isharonov@elvees.com>
2020-09-22 15:00:02 +0300

    package/openvx: Update version to 1.0.2
    This version of OpenVX doesn't use DSP, only CPU is used.
    
    Issue: #MCOM02SW-1727
    Change-Id: Ife188c1f6588f060c74bd4785262bd9a67061029

--- package/openvx/openvx.mk
+++ package/openvx/openvx.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 
-OPENVX_MAJOR_VERSION = 0
-OPENVX_VERSION = $(OPENVX_MAJOR_VERSION).1.1
+OPENVX_MAJOR_VERSION = 1
+OPENVX_VERSION = $(OPENVX_MAJOR_VERSION).0.2
 OPENVX_LICENSE = custom
 OPENVX_SITE = $(BR2_EXTERNAL_MCOM02_PATH)/package/openvx
 OPENVX_SITE_METHOD = local
@@ -13,14 +13,16 @@ OPENVX_INSTALL_STAGING = YES
 OPENVX_DEPENDENCIES = openvx-headers
 
 define OPENVX_INSTALL_CMD
-	$(INSTALL) -D -m 0755 $(@D)/libopenvx.so.$(OPENVX_VERSION) $(1)/usr/lib/libopenvx.so.$(OPENVX_VERSION)
-	ln -sf libopenvx.so.$(OPENVX_VERSION) $(1)/usr/lib/libopenvx.so
-	ln -sf libopenvx.so.$(OPENVX_VERSION) $(1)/usr/lib/libopenvx.so.$(OPENVX_MAJOR_VERSION)
+	$(INSTALL) -D -m 0755 $(@D)/libopenvx_wrapper.so.$(OPENVX_VERSION) $(1)/usr/lib/libopenvx_wrapper.so.$(OPENVX_VERSION)
+	ln -sf libopenvx_wrapper.so.$(OPENVX_VERSION) $(1)/usr/lib/libopenvx_wrapper.so
+	ln -sf libopenvx_wrapper.so.$(OPENVX_VERSION) $(1)/usr/lib/libopenvx_wrapper.so.$(OPENVX_MAJOR_VERSION)
+	$(INSTALL) -D -m 0755 $(@D)/libopenvx_kernels.so.$(OPENVX_VERSION) $(1)/usr/lib/libopenvx_kernels.so.$(OPENVX_VERSION)
+	ln -sf libopenvx_kernels.so.$(OPENVX_VERSION) $(1)/usr/lib/libopenvx_kernels.so
+	ln -sf libopenvx_kernels.so.$(OPENVX_VERSION) $(1)/usr/lib/libopenvx_kernels.so.$(OPENVX_MAJOR_VERSION)
 endef
 
 define OPENVX_INSTALL_STAGING_CMDS
 	$(call OPENVX_INSTALL_CMD,$(STAGING_DIR))
-	$(INSTALL) -D -m 0644 $(@D)/openvx-extended.h $(STAGING_DIR)/usr/include/openvx_extended.h
 endef
 
 OPENVX_INSTALL_TARGET_CMDS = $(call OPENVX_INSTALL_CMD,$(TARGET_DIR))
