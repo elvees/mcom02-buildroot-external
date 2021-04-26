bfdd41ec93638f025ef89ab1511dd61cc5bef15d
Anton Leontiev <aleontiev@elvees.com>
2021-04-26 10:28:55 +0300

    package/m2m-test: Add libdrm and sdl2 dependencies
    With recent addition of dec-vpout, libdrm and sdl2 are required to
    build m2m-test package.
    
    Issue: #MCOM02SW-1718
    Change-Id: Ice98070e71699f8b919fe9a3aec5069a9e1d4637

--- package/m2m-test/m2m-test.mk
+++ package/m2m-test/m2m-test.mk
@@ -10,4 +10,12 @@ M2M_TEST_SITE_METHOD = git
 M2M_TEST_LICENSE = GPLv2
 M2M_TEST_DEPENDENCIES = ffmpeg
 
+ifeq ($(BR2_PACKAGE_LIBDRM),y)
+M2M_TEST_DEPENDENCIES += libdrm
+
+ifeq ($(BR2_PACKAGE_SDL2),y)
+M2M_TEST_DEPENDENCIES += sdl2
+endif
+endif
+
 $(eval $(cmake-package))
