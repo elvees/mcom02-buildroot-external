6aba2d1d6ddf9e5dbb6eea304debcea21d155ee8
Vitaliy Lotorev <vlotorev@elvees.com>
2024-06-10 18:05:13 +0300

    mcom-python-linux-tests: Rename repo to mcom02/linux-tests
    Issue: #MCOM02SW-2235
    Change-Id: I310e8d813e2aadf7844b0d110aa1dda62944ff9d

--- package/mcom/mcom-python-linux-tests/mcom-python-linux-tests.mk
+++ package/mcom/mcom-python-linux-tests/mcom-python-linux-tests.mk
@@ -5,7 +5,7 @@
 ################################################################################
 
 MCOM_PYTHON_LINUX_TESTS_VERSION = master
-MCOM_PYTHON_LINUX_TESTS_SITE = ssh://gerrit.elvees.com:29418/tools/mcom-linux-tests
+MCOM_PYTHON_LINUX_TESTS_SITE = ssh://gerrit.elvees.com:29418/mcom02/linux-tests
 MCOM_PYTHON_LINUX_TESTS_SITE_METHOD = git
 MCOM_PYTHON_LINUX_TESTS_LICENSE = BSD-2c
 MCOM_PYTHON_LINUX_TESTS_DEPENDENCIES = python3
