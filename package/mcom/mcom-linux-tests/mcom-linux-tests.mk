22b42314ac9adf176496e59ef351191c2d9472c3
Maksim Tarelov <mtarelov@elvees.com>
2024-12-19 16:34:26 +0300

    mcom_defconfig: mcom-linux-tests: Add ...MALI_UTILS option
    Mali drivers are not supported on Linux 5.4, so new option
    BR2_PACKAGE_MCOM_LINUX_TESTS_MALI_UTILS is added to disable installing
    Mali demos on Linux 5.4.
    
    mcom02/linux-tests repository also added CMake option to skip
    installing Mali utils.
    
    We enable mcom_defconfig by default, but disalbe ...MALI_UTILS in
    linux54.fragment.
    
    Depends-On: https://gerrit.elvees.com/c/mcom02/linux-tests/+/49492
    Issue: #MCOM02SW-2299
    Change-Id: Ie31f8c01f25644b7abd6a02a0c5b0c82ac281b96

--- package/mcom/mcom-linux-tests/mcom-linux-tests.mk
+++ package/mcom/mcom-linux-tests/mcom-linux-tests.mk
@@ -9,4 +9,8 @@ MCOM_LINUX_TESTS_SITE = ssh://gerrit.elvees.com:29418/mcom02/linux-tests
 MCOM_LINUX_TESTS_SITE_METHOD = git
 MCOM_LINUX_TESTS_LICENSE = BSD-2c
 
+ifeq ($(BR2_PACKAGE_MCOM_LINUX_TESTS_MALI_UTILS),y)
+    MCOM_LINUX_TESTS_OPTS += -DWITH_MALI_UTILS=ON
+endif
+
 $(eval $(cmake-package))
